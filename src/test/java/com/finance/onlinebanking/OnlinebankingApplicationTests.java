aab7e4f3989d28f67458a91464f017efe00da31d
Jaster25 <js_328@naver.com>
2023-01-19 14:49:49 +0900

    [#1] 프로젝트 디렉토리 구조 추가

---
+++ src/test/java/com/finance/onlinebanking/OnlinebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.onlinebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
