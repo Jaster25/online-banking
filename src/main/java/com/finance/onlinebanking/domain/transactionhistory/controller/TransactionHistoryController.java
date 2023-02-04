3137967e0e8ea00c64db1fe36eed54b39f1ff4e9
Jaster25 <js_328@naver.com>
2023-02-04 20:17:52 +0900

    [#27] Feature: 거래내역 관련 API 문서화 적용

--- src/main/java/com/finance/onlinebanking/domain/transactionhistory/controller/TransactionHistoryController.java
+++ src/main/java/com/finance/onlinebanking/domain/transactionhistory/controller/TransactionHistoryController.java
@@ -2,6 +2,12 @@ package com.finance.onlinebanking.domain.transactionhistory.controller;
 
 import com.finance.onlinebanking.domain.transactionhistory.dto.TransactionHistoryResponseDto;
 import com.finance.onlinebanking.domain.transactionhistory.service.TransactionHistoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "거래내역 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/transactions")
@@ -17,8 +24,11 @@ public class TransactionHistoryController {
 
     private final TransactionHistoryService transactionHistoryService;
 
+    @Operation(summary = "거래내역 상세 조회", description = "거래내역을 상세 조회한다.")
+    @ApiResponse(responseCode = "200", description = "successful operation",
+            content = @Content(schema = @Schema(implementation = TransactionHistoryResponseDto.class)))
     @GetMapping("/{transactionId}")
-    public ResponseEntity<TransactionHistoryResponseDto> getTransactionHistoryApi(@PathVariable Long transactionId) {
+    public ResponseEntity<TransactionHistoryResponseDto> getTransactionHistoryApi(@Parameter(description = "거래내역 아이디") @PathVariable Long transactionId) {
         TransactionHistoryResponseDto transactionHistoryResponseDto = transactionHistoryService.getTransactionHistory(transactionId);
         return ResponseEntity.status(HttpStatus.OK).body(transactionHistoryResponseDto);
     }
--- src/main/java/com/finance/onlinebanking/domain/transactionhistory/dto/TransactionHistoryResponseDto.java
+++ src/main/java/com/finance/onlinebanking/domain/transactionhistory/dto/TransactionHistoryResponseDto.java
@@ -1,6 +1,7 @@
 package com.finance.onlinebanking.domain.transactionhistory.dto;
 
 import com.finance.onlinebanking.domain.transactionhistory.entity.TransactionHistoryEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,20 +10,28 @@ import java.time.LocalDateTime;
 @Getter
 public class TransactionHistoryResponseDto {
 
+    @Schema(description = "거래내역 아이디")
     private final Long id;
 
+    @Schema(description = "출금 계좌 번호")
     private final String withdrawAccountNumber;
 
+    @Schema(description = "입금 계좌 번호")
     private final String depositAccountNumber;
 
+    @Schema(description = "금액")
     private final Long amount;
 
+    @Schema(description = "메모")
     private final String memo;
 
+    @Schema(description = "수수료")
     private final Long commission;
 
+    @Schema(description = "생성일")
     private final LocalDateTime createdAt;
 
+    @Schema(description = "수정일")
     private final LocalDateTime updatedAt;
 
 
